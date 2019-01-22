b9a8b3e1a6078664499364ea83be4d1a5a6af8ab
Rudy Zaccaria <46543175+rudyzac@users.noreply.github.com>
2019-01-22 23:27:51 +0100

    Added support for Chrome debugging

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -10,11 +10,15 @@ import { ProductRepository } from '../model/product.repository';
 export class StoreComponent {
 
     public selectedCategory = null;
+    public productsPerPage = 4;
+    public selectedPage = 1;
 
     constructor(private repository: ProductRepository) { }
 
     get products(): Product[] {
-        return this.repository.getProducts(this.selectedCategory);
+        const productIndex = (this.selectedPage - 1) * this.productsPerPage;
+        return this.repository.getProducts(this.selectedCategory)
+            .slice(productIndex, productIndex + this.productsPerPage);
     }
 
     get categories(): string[] {
@@ -23,6 +27,15 @@ export class StoreComponent {
 
     changeCategory(newCategory?: string) {
         this.selectedCategory = newCategory;
+    }
+
+    changePage(newPage: number) {
+        this.selectedPage = newPage;
+    }
+
+    changePageSize(newSize: number) {
+        this.productsPerPage = Number(newSize);
+        this.changePage(1);
     }
 
 }
