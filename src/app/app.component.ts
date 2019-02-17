cecd4c5d4b401a06ddd3bb73ca2cbc1d0695a47a
Rudy Zaccaria <46543175+rudyzac@users.noreply.github.com>
2019-02-17 12:44:40 +0100

    Added routing configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'app',
-  template: '<store></store>'
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { StoreModule } from './store/store.module';
+import { StoreComponent } from './store/store.component';
+import { CartDetailComponent } from './store/cartDetail.component';
+import { CheckoutComponent } from './store/checkout.component';
+import { RouterModule } from '@angular/core';
 
 @NgModule({
   declarations: [
@@ -9,7 +13,13 @@ import { StoreModule } from './store/store.module';
   ],
   imports: [
     BrowserModule,
-    StoreModule
+    StoreModule,
+    RouterModule.forRoot([
+      { path: 'store', component: StoreComponent },
+      { path: 'cart', component: CartDetailComponent },
+      { path: 'checkout', component: CheckoutComponent },
+      { path: '**', redirectTo: '/store' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
